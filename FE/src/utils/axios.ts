c32cd4b597b887b89e550f4e7466326ed1914e51
Brandon Quon <bqa7@sfu.ca>
2025-06-30 20:44:03 -0700

    push to reset vercel

--- FE/src/utils/axios.ts
+++ FE/src/utils/axios.ts
@@ -9,7 +9,6 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
   (config) => {
     const token = tokenManager.getToken();
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
